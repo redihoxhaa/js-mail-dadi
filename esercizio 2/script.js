b639ebc7c62a1a79dabd250f928d748184f77ea2
redihoxhaa <redi.hoxha1996@gmail.com>
2023-11-09 18:07:52 +0100

    new: inserito esercizio bonus

--- esercizio 2/script.js
+++ esercizio 2/script.js
@@ -12,3 +12,44 @@ if (userNumber > cpuNumber) {
 } else {
     console.log("Partita finita in parità!");
 }
+
+
+
+// Togli il commento qui sotto e alla fine per sbloccare l'esercizio bonus, e commenta quello superiore.
+
+/*
+
+// In questo programma, viene chiesto all'utente quante partite vuole fare, 
+// e i punteggi vengono accumulati in due variabili che verranno confrontate alla fine delle partite.
+// Vengono implementati dei controlli sull'input
+
+const playsCounter = prompt("Inserisci il numero delle giocate che vuoi fare")
+let userTotal = 0;
+let cpuTotal = 0;
+
+if (playsCounter > 0) {
+
+    for (let cycleNumber = 0; cycleNumber < playsCounter; cycleNumber++) {
+        const userNumber = Math.floor((Math.random() * 6) + 1);
+        const cpuNumber = Math.floor((Math.random() * 6) + 1);
+        userTotal += userNumber;
+        cpuTotal += cpuNumber;
+    }
+
+    console.log("Il totale del giocatore è " + userTotal);
+    console.log("Il totale della CPU è " + cpuTotal);
+
+    if (userTotal > cpuTotal) {
+        console.log("Ha vinto l'utente!");
+    } else if (cpuTotal > userTotal) {
+        console.log("Ha vinto la CPU!");
+    } else {
+        console.log("Partita finita in parità!");
+    }
+} else if (playsCounter < 0) {
+    console.log("Non puoi fare giocate negative scemo!")
+} else if (typeof (playsCounter) === "string") {
+    console.log("Ti supplico scrivi il numero in cifre...")
+}
+
+*/

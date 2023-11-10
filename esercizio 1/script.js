ba5b6f6c685c968343e4a7cdd64c599897ebc8f6
redihoxhaa <redi.hoxha1996@gmail.com>
2023-11-10 10:19:27 +0100

    feat: aggiunta statusCheck

--- esercizio 1/script.js
+++ esercizio 1/script.js
@@ -2,12 +2,17 @@
 
 const emailsList = ["erjembo@ew.bz", "fut@tomihmep.cf", "zaj@tusef.au", "vu@bi.mc", "gesozebu@de.pn", "mues@zofisuc.ky", "apigo@tohucbak.org", "litiduz@dulgurgi.lb", "edaam@kut.pg", "lomafsel@tepvib.pr", "uma@fahi.gh", "cu@ohefi.org", "fazefuga@awses.fk", "mavsifco@ik.eh", "cezam@pu.lt", "mibpel@kotceiro.ac", "soriwa@gamdet.fj", "ap@nizgoumu.gi", "sanhi@re.dm", "vundemi@juku.kh", "visiret@big.rs", "os@nog.in", "behbow@isuig.tn", "bitgivob@saowpum.fj", "du@ru.nr"];
 const userEmail = prompt("Inserisci la tua email");
+let statusCheck = false;
 
 for (let counter = 0; counter < emailsList.length; counter++) {
     if (userEmail === emailsList[counter]) {
-        console.log("Sei autorizzato ad entrare nel nostro database di armi nucleari!");
+        statusCheck = true;
         break;
-    } else {
-        console.log("Mi spiace, devi riprogrammare il tuo attacco terroristico alla prossima volta :(")
     }
+}
+
+if (statusCheck === true) {
+    console.log("Sei autorizzato ad entrare nel nostro database di armi nucleari!");
+} else {
+    console.log("Mi spiace, devi riprogrammare il tuo attacco terroristico alla prossima volta :(");
 }
